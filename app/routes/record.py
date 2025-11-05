57e3fdc70e4f21ca5dd167462a1a17b1143a49da
andriy8075 <andriy8075@gmail.com>
2025-11-05 09:50:59 +0200

    record validation on get and delete

--- app/routes/record.py
+++ app/routes/record.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, jsonify, request
 from marshmallow import ValidationError
+from sqlalchemy.orm import joinedload
 from app.models.record import Record
 from app.schemas.record_schema import RecordSchema, RecordCreateSchema
 from flask_jwt_extended import (
     jwt_required,
+    get_jwt_identity
 )
 
 record_bp = Blueprint('record', __name__)
@@ -32,9 +34,12 @@ def create_record():
 @record_bp.route('/record/<int:id>', methods=['GET'])
 @jwt_required()
 def get_record(id):
-    record = Record.get_by_id(id)
+    record = Record.query.options(joinedload(Record.category)).get(id)
     if record is None:
         return jsonify({'error': 'Record not found'}), 404
+
+    if record.category.user_id != int(get_jwt_identity()):
+        return jsonify({'error': 'Record does not belong to the current user'}), 403
 
     record_schema = RecordSchema()
     return jsonify(record_schema.dump(record)), 200
@@ -42,7 +47,12 @@ def get_record(id):
 @record_bp.route('/record/<int:id>', methods=['DELETE'])
 @jwt_required()
 def delete_record(id):
-    if Record.delete(id):
-        return jsonify({'message': 'Record deleted successfully'}), 200
-    else:
+    record = Record.query.options(joinedload(Record.category)).get(id)
+    if record is None:
         return jsonify({'error': 'Record not found'}), 404
+
+    if record.category.user_id != int(get_jwt_identity()):
+        return jsonify({'error': 'Record does not belong to the current user'}), 403
+
+    Record.delete(id)
+    return jsonify({'message': 'Record deleted successfully'}), 200
--- app/schemas/record_schema.py
+++ app/schemas/record_schema.py
@@ -4,6 +4,7 @@ from app.config.models_validation.record import min_sum
 from app.config.currencies import SUPPORTED_CURRENCIES
 from app.models.user import User
 from app.models.category import Category
+from flask_jwt_extended import get_jwt_identity
 
 
 class RecordSchema(Schema):
@@ -21,8 +22,12 @@ class RecordCreateSchema(Schema):
     date_time = fields.DateTime(load_default=datetime.utcnow, allow_none=True)
     
     @validates_schema
-    def validate_category_exists(self, data, **kwargs):
+    def validate_category_exists_and_belongs_to_user(self, data, **kwargs):
         category = Category.query.get(data['category_id'])
         if category is None:
             raise ValidationError('Category with specified ID does not exist', field_name='category_id')
+
+        authenticated_user_id = int(get_jwt_identity())
+        if category.user_id != authenticated_user_id:
+            raise ValidationError('Category does not belong to the current user', field_name='category_id')
     
